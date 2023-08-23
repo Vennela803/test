7a3edfb54a83f860db949120ddb3b836d7148e70
vennela0803 <vennelagodugu@gmail.com>
2023-08-23 22:50:34 +0530

    added

---
+++ A.java
@@ -0,0 +1,16 @@
+class A
+{
+public static void  main(String[]args)
+{
+int x=24;
+double y=Math.sqrt(x);
+if(y==(int)y)
+{
+System.out.println("perfect");
+}
+else
+{
+System.out.println("not perfect");
+}
+}
+}

113e0d88299609f584a622778b8ccce808f1e829
vennela0803 <vennelagodugu@gmail.com>
2023-08-23 22:59:56 +0530

    adding

--- A.java
+++ A.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class A
 {
 public static void  main(String[]args)
@@ -13,4 +14,21 @@ else
 System.out.println("not perfect");
 }
 }
+=======
+class A
+{
+public static void  main(String[]args)
+{
+int x=24;
+double y=Math.sqrt(x);
+if(y==(int)y)
+{
+System.out.println("perfect");
+}
+else
+{
+System.out.println("not perfect");
+}
+}
+>>>>>>> 109fe60c951f90494b0390989a4364b7d9bd685e
 }
